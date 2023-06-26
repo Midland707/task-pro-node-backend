467ed758b02e2eef87e889a318dcfed14db65eb6
Midland707 <rus14job@ukr.net>
2023-06-26 15:12:15 +0300

    update getBoardById

--- controllers/boards/getBoardById.js
+++ controllers/boards/getBoardById.js
@@ -1,10 +1,12 @@
 const { Board } = require("../../models");
+const { Column } = require("../../models");
+const { Card } = require("../../models");
 const { HttpError } = require("../../helpers");
 
 const getBoardById = async (req, res) => {
-  const { id } = req.params;
-  const { _id: owner } = req.user;
-  const result = await Board.findOne({ _id: id, owner });
+  const { id: columnOwner } = req.params;
+  const result = await Column.find({ columnOwner });
+  const result1 = await Column.find({ cardOwner });
   if (!result) throw HttpError(404);
   res.json(result);
 };
