0be56f944f4c402374e1fb1f6258a3b66ecdbbef
Midland707 <rus14job@ukr.net>
2023-06-27 23:42:29 +0300

    bugfix getBoardById

--- controllers/boards/getBoardById.js
+++ controllers/boards/getBoardById.js
@@ -8,12 +8,12 @@ const getBoardById = async (req, res) => {
   const columns = await Column.find({ columnOwner });
   const cards = await Promise.all(
     columns.map(async ({ _id: cardOwner }) => {
-      return await Card.find({ cardOwner }).populate("cardOwner");
+      return await Card.find({ cardOwner });
     })
   );
 
   if (!cards) throw HttpError(404);
-  res.json(cards);
+  res.json({ columns, cards: cards.flat() });
 };
 
 module.exports = getBoardById;
