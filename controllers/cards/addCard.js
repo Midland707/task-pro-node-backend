ab0cfbad619104aa59f6ba879292cdc9a660be9c
Midland707 <rus14job@ukr.net>
2023-06-27 17:37:14 +0300

    add card to array cards

--- controllers/cards/addCard.js
+++ controllers/cards/addCard.js
@@ -1,9 +1,21 @@
+const { Column } = require("../../models");
 const { Card } = require("../../models");
 
 const addCard = async (req, res) => {
+  // const { id: cardOwner } = req.params;
+  // const result = await Card.create({ ...req.body, cardOwner });
+  // res.status(201).json(result);
+
   const { id: cardOwner } = req.params;
-  const result = await Card.create({ ...req.body, cardOwner });
-  res.status(201).json(result);
+  const column = await Column.findById(cardOwner);
+  if (!column) {
+    return res.status(404).json({ error: "Column not found" });
+  }
+  const card = new Card({ ...req.body, cardOwner });
+  await card.save();
+  column.cards.push(card);
+  await column.save();
+  res.status(201).json(card);
 };
 
 module.exports = addCard;
