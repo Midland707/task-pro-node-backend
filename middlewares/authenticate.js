20767a47b9260c20ff659ff8252a6bef3a637196
Midland707 <rus14job@ukr.net>
2023-07-03 22:29:48 +0300

    bugfix  change token of condition

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -16,8 +16,7 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
 
     const user = await User.findById(id);
-    if (!user || !user.token || user.token !== token)
-      next(HttpError(401, "Not authorized"));
+    if (!user || !user.token) next(HttpError(401, "Not authorized"));
 
     req.user = user;
     next();
